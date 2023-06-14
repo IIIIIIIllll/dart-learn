2b6b4ff00928961eab5e69ee4255cf1d8583b65f
Zahar <53586791+allin17@users.noreply.github.com>
2023-06-14 18:40:41 +0300

    Update task_1.dart

--- 1_dart_concepts/1_1_mixins/task_1.dart
+++ 1_dart_concepts/1_1_mixins/task_1.dart
@@ -1,4 +1,14 @@
+extension StringParsing on DateTime {
+    String parseString() {
+      String s = toString();
+      return s.substring(0, s.length - 4);
+    }
+  }
+
 void main() {
   // Implement an extension on [DateTime], returning a [String] in format of
   // `YYYY.MM.DD hh:mm:ss` (e.g. `2023.01.01 00:00:00`).
+  
+  final date = DateTime.now();
+  print(date.parseString());
 }
