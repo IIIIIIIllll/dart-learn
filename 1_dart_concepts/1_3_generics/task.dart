ef7ef45b96d89a3c6da434d32deedeb1cb914953
Zahar <53586791+allin17@users.noreply.github.com>
2023-06-17 00:00:30 +0300

    Update task.dart generic

--- 1_dart_concepts/1_3_generics/task.dart
+++ 1_dart_concepts/1_3_generics/task.dart
@@ -1,4 +1,25 @@
+T findMax<T extends Comparable>(List<T> list) {
+  if (list.isEmpty) {
+    throw ArgumentError('The list cannot be empty.');
+  }
+
+  T max = list[0];
+
+  for (int i = 1; i < list.length; i++) {
+    if (list[i].compareTo(max) > 0) {
+      max = list[i];
+    }
+  }
+
+  return max;
+}
+
 void main() {
-  // Implement a method, returning the maximum element from a `Comparable` list.
-  // You must use generics to allow different types usage with that method.
+  List<int> numbers = [5, 2, 10, 8, 3];
+  int maxNumber = findMax(numbers);
+  print('Maximum number: $maxNumber');
+
+  List<String> names = ['John', 'Alice', 'Bob', 'David'];
+  String maxName = findMax(names);
+  print('Maximum name: $maxName');
 }
