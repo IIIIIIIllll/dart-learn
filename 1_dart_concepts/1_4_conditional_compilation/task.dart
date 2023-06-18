d458b5a9c8d2d41018685364e4657bdb4142034c
Zahar <53586791+allin17@users.noreply.github.com>
2023-06-18 17:27:32 +0300

    Update task.dart

--- 1_dart_concepts/1_4_conditional_compilation/task.dart
+++ 1_dart_concepts/1_4_conditional_compilation/task.dart
@@ -1,5 +1,60 @@
+import 'dart:io';
+
+
+class CustomDateTime {
+  late int year;
+  late int month;
+  late int day;
+  late int hour;
+  late int minute;
+  late int second;
+  late int microsecond;
+
+  CustomDateTime({
+    required this.year,
+    required this.month,
+    required this.day,
+    required this.hour,
+    required this.minute,
+    required this.second,
+    required this.microsecond,
+  });
+
+  @override
+  String toString() {
+    return '$year-$month-$day $hour:$minute:$second.$microsecond';
+  }
+}
+
 void main() {
-  // Create a native and web implementations for a custom [DateTime], supporting
-  // microseconds. Use conditional compilation to export the class for general
-  // use on any platform.
+  CustomDateTime dateTime;
+
+  // Platform-specific implementation
+  if (Platform.isAndroid || Platform.isIOS) {
+    // Native implementation
+    final now = DateTime.now();
+    dateTime = CustomDateTime(
+      year: now.year,
+      month: now.month,
+      day: now.day,
+      hour: now.hour,
+      minute: now.minute,
+      second: now.second,
+      microsecond: now.microsecond,
+    );
+  } else {
+    
+    // Web implementation
+    dateTime = CustomDateTime(
+      year: 2023,
+      month: 6,
+      day: 6,
+      hour: 12,
+      minute: 0,
+      second: 0,
+      microsecond: 10,
+    );
+  }
+
+  print(dateTime);
 }
