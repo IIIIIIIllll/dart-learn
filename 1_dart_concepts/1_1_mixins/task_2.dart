d6ec785ba4d488238efd656c437b7bcdbdc001fa
Zahar <53586791+allin17@users.noreply.github.com>
2023-06-18 15:40:16 +0300

    Update task_2.dart

--- 1_dart_concepts/1_1_mixins/task_2.dart
+++ 1_dart_concepts/1_1_mixins/task_2.dart
@@ -1,7 +1,48 @@
+class Text {
+  final String text;
+  
+  Text(this.text);
+}
+
+class Link {
+  final String link;
+  
+  Link(this.link);
+}
+
+extension StringExtension on String {
+  List<dynamic> parseLinks() {
+    RegExp linkPattern = RegExp(r'(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+');
+    List<dynamic> result = [];
+
+    var matches = linkPattern.allMatches(this);
+    var currentIndex = 0;
+
+    for (var match in matches) {
+      if (match.start > currentIndex) {
+        result.add(Text(this.substring(currentIndex, match.start)));
+      }
+      result.add(Link(match.group(0)!));
+      currentIndex = match.end;
+    }
+
+    if (currentIndex < this.length) {
+      result.add(Text(this.substring(currentIndex)));
+    }
+
+    return result;
+  }
+}
+
 void main() {
-  // Implement an extension on [String], parsing links from a text.
-  //
-  // Extension should return a [List] of texts and links, e.g.:
-  // - `Hello, google.com, yay` ->
-  //   [Text('Hello, '), Link('google.com'), Text(', yay')].
+  String text = 'Hello, google.com, yay';
+  List<dynamic> parsedResult = text.parseLinks();
+  
+  for (var item in parsedResult) {
+    if (item is Text) {
+      print('Text: ${item.text}');
+    } else if (item is Link) {
+      print('Link: ${item.link}');
+    }
+  }
 }
