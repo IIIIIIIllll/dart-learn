4b57142ad49abc362375d01b8df632fdb4602beb
Zahar <53586791+allin17@users.noreply.github.com>
2023-06-21 14:58:45 +0300

    Update task_3.dart mixins

--- 1_dart_concepts/1_1_mixins/task_3.dart
+++ 1_dart_concepts/1_1_mixins/task_3.dart
@@ -1,6 +1,16 @@
 /// Object equipable by a [Character].
 abstract class Item {}
 
+/// Mixin for items that provide damage.
+mixin Weapon on Item {
+  int damage = 0;
+}
+
+/// Mixin for items that provide defense.
+mixin Armor on Item {
+  int defense = 0;
+}
+
 /// Entity equipping [Item]s.
 class Character {
   Item? leftHand;
@@ -16,31 +26,117 @@ class Character {
 
   /// Returns the total damage of this [Character].
   int get damage {
-    // TODO: Implement me.
-    return 0;
+    int totalDamage = 0;
+    for (var item in equipped) {
+      if (item is Weapon) {
+        totalDamage += item.damage;
+      }
+    }
+    return totalDamage;
   }
 
   /// Returns the total defense of this [Character].
   int get defense {
-    // TODO: Implement me.
-    return 0;
+    int totalDefense = 0;
+    for (var item in equipped) {
+      if (item is Armor) {
+        totalDefense += item.defense;
+      }
+    }
+    return totalDefense;
   }
 
   /// Equips the provided [item], meaning putting it to the corresponding slot.
   ///
   /// If there's already a slot occupied, then throws a [OverflowException].
   void equip(Item item) {
-    // TODO: Implement me.
+    if (item is Weapon) {
+      if (leftHand == null) {
+        leftHand = item;
+      } else if (rightHand == null) {
+        rightHand = item;
+      } else {
+        throw OverflowException();
+      }
+    } else if (item is Armor) {
+      if (item is Hat && hat == null) {
+        hat = item;
+      } else if (item is Helmet && hat == null) {
+        hat = item;
+      } else if (item is Torso && torso == null) {
+        torso = item;
+      } else if (item is Legs && legs == null) {
+        legs = item;
+      } else if (item is Shoes && shoes == null) {
+        shoes = item;
+      } else {
+        throw OverflowException();
+      }
+    }
   }
 }
 
 /// [Exception] indicating there's no place left in the [Character]'s slot.
 class OverflowException implements Exception {}
 
+/// Example weapon.
+class Sword extends Item with Weapon {
+  @override
+  int damage;
+
+  Sword(this.damage);
+}
+
+/// Example armor.
+class Helmet extends Item with Armor {
+  @override
+  int defense;
+
+  int getDefense() {
+    return this.defense;
+  }
+
+  Helmet(this.defense);
+}
+
+class Hat extends Item with Armor {
+  @override
+  int defense;
+
+  Hat(this.defense);
+}
+
+class Torso extends Item with Armor {
+  @override
+  int defense;
+
+  Torso(this.defense);
+}
+
+class Legs extends Item with Armor {
+  @override
+  int defense;
+
+  Legs(this.defense);
+}
+
+class Shoes extends Item with Armor {
+  @override
+  int defense;
+
+  Shoes(this.defense);
+}
+
 void main() {
-  // Implement mixins to differentiate [Item]s into separate categories to be
-  // equipped by a [Character]: weapons should have some damage property, while
-  // armor should have some defense property.
-  //
-  // [Character] can equip weapons into hands, helmets onto hat, etc.
+  Character character = Character();
+  Sword sword = Sword(10);
+  Helmet helmet = Helmet(5);
+  Shoes shoes = Shoes(2);
+
+  character.equip(sword);
+  character.equip(helmet);
+  character.equip(shoes);
+
+  print("Total damage: ${character.damage}");
+  print("Total defense: ${character.defense}");
 }
