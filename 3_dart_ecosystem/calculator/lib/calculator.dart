ff05962b2dcdb3dc3b89141d65b7179476a03834
Zahar <53586791+allin17@users.noreply.github.com>
2023-06-22 15:53:06 +0300

    calaculator package

---
+++ 3_dart_ecosystem/calculator/lib/calculator.dart
@@ -0,0 +1,17 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/calculator_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
+class Calculator {
+  int sum(int a, int b) => a + b;
+
+  int subtract(int a, int b) => a - b;
+
+  int multiply(int a, int b) => a * b;
+
+  double divide(double a, double b) => a / b;
+}
