2e2359d648fb3c3e42f32284bf4456c29d0c7f36
Zahar <53586791+allin17@users.noreply.github.com>
2023-06-23 17:11:50 +0300

    json serializable person task

---
+++ 3_dart_ecosystem/3_6_codegen/task/lib/person.dart
@@ -0,0 +1,14 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'person.g.dart';
+
+@JsonSerializable()
+class Person {
+  const Person({required this.name, required this.birthday});
+
+  final String name;
+  final DateTime birthday;
+
+  factory Person.fromJson(Map<String, dynamic> json) => _$PersonFromJson(json);
+  Map<String, dynamic> toJson() => _$PersonToJson(this);
+}
